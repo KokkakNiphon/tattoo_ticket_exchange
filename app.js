73fec059546e4dc59b74a8339c7eaf8f2a4961ff
Niphon Prasopphol <kokkakniphon@gmail.com>
2023-12-05 01:39:21 +0700

    -_-

--- app.js
+++ app.js
@@ -1,61 +1,74 @@
-// Assuming firebaseConfig is defined earlier in your code or in another file
-document.querySelectorAll('area').forEach(area => {
-  area.addEventListener('click', function(e) {
-    e.preventDefault();
-    const zone = this.dataset.zone;
-    checkSeatStatus(zone);
-  });
-});
+let seatData = []; // This will hold the data from the Google Spreadsheet
 
-function checkSeatStatus(zone) {
-  db.collection('seats').doc(zone).get().then((doc) => {
-    const seatStatus = doc.exists ? (doc.data().isTaken ? 'Taken' : 'Available') : 'No data';
-    document.getElementById('seatStatus').innerText = `Seat Status for ${zone}: ${seatStatus}`;
-  }).catch((error) => {
-    console.error("Error getting document:", error);
-  });
+function fetchSpreadsheetData(spreadsheetId, apiKey) {
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/A:Z?key=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Assuming the first row contains column headers: Zone, Seat, Contact
+      const rows = data.values.slice(1); // Skip the header row
+      seatData = rows.map(row => ({ โซนที่มี: row[0], ที่นั่ง: row[1], วันที่: row[2], คอนแท็กติดต่อ: row[3], ที่หาแลก: row[4], หมายเหตุ: row[5]}));
+      console.log(seatData); // Now seatData is populated with the spreadsheet data
+    })
+    .catch(error => console.error('Error fetching data:', error));
 }
 
-// This function will create a circle overlay for debugging purposes
-function createCircleOverlay(coords) {
-  const [x, y, radius] = coords.split(',').map(Number);
-  const overlay = document.createElement('div');
-  overlay.style.position = 'absolute';
-  overlay.style.left = `${x - radius}px`;
-  overlay.style.top = `${y - radius}px`;
-  overlay.style.width = `${radius * 2}px`;
-  overlay.style.height = `${radius * 2}px`;
-  overlay.style.borderRadius = '50%'; // Makes the div circular
-  overlay.style.backgroundColor = 'rgba(0, 255, 0, 0.5)'; // Green with transparency
-  overlay.style.zIndex = '10'; // Ensure it's on top of the image but below any popups
-  overlay.style.pointerEvents = 'none'; // Allows clicking through the div
-  return overlay;
+// Call fetchSpreadsheetData with your Spreadsheet ID and API Key
+fetchSpreadsheetData('1_sanAKZ1OKOpaaLRXA-ebKO_-pJHL7Ph2Qw_XM3XgUQ', 'AIzaSyDX70rFBT0ZFe1eQYhctx9oGHKoT0MBRKI');
+
+// Function to generate a table based on the zone
+function generateTableForZone(zone) {
+  const table = document.createElement('table');
+  table.style.width = '100%';
+  table.style.borderCollapse = 'collapse';
+  table.innerHTML = `<tr><th>ที่นั่ง</th><th>วันที่</th><th>คอนแท็กติดต่อ</th><th>ที่หาแลก</th><th>หมายเหตุ</th></tr>`;
+
+  const rowData = seatData.find(row => row.โซนที่มี === zone);
+  if (rowData) {
+    const row = table.insertRow();
+    const seatCell = row.insertCell();
+    const dateCell = row.insertCell();
+    const contactCell = row.insertCell();
+    const targetCell = row.insertCell();
+    const noteCell = row.insertCell();
+    seatCell.textContent = rowData.ที่นั่ง;
+    dateCell.textContent = rowData.วันที่;
+    contactCell.textContent = rowData.คอนแท็กติดต่อ;
+    targetCell.textContent = rowData.ที่หาแลก;
+    noteCell.textContent = rowData.หมายเหตุ;
+    seatCell.style.border = '1px solid white';
+    dateCell.style.border = '1px solid white';
+    contactCell.style.border = '1px solid white';
+    targetCell.style.border = '1px solid white';
+    noteCell.style.border = '1px solid white';
+  } else {
+    // In case no data is found for the zone
+    table.innerHTML += `<tr><td colspan="2">No data available for zone ${zone}</td></tr>`;
+  }
+
+  return table;
+}
+
+// Modify the existing checkSeatStatus function
+function checkSeatStatus(zone) {
+  console.log("Zone clicked:", zone);
+  if (!seatData.length) {
+    console.log("Data is not loaded yet.");
+    return;
+  }
+  const seatStatusDiv = document.getElementById('seatStatus');
+  seatStatusDiv.innerHTML = ''; // Clear previous data
+  seatStatusDiv.appendChild(generateTableForZone(zone));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const imageContainer = document.querySelector('.image-container');
-    const cursorCoordsText = document.createElement('div');
-    cursorCoordsText.id = 'cursor-coords';
-    cursorCoordsText.style.position = 'absolute';
-    cursorCoordsText.style.padding = '5px';
-    cursorCoordsText.style.backgroundColor = 'white';
-    cursorCoordsText.style.color = 'black';
-    cursorCoordsText.style.border = '1px solid black';
-    cursorCoordsText.style.pointerEvents = 'none'; // Ensure text doesn't interfere with mouse events
-    cursorCoordsText.style.zIndex = '1000';
-    document.body.appendChild(cursorCoordsText);
-  
-    imageContainer.addEventListener('mousemove', function(e) {
-      const rect = this.getBoundingClientRect();
-      const x = e.clientX - rect.left; // x position within the element
-      const y = e.clientY - rect.top;  // y position within the element
-      cursorCoordsText.textContent = `Coords: (${Math.round(x)}, ${Math.round(y)})`;
-      cursorCoordsText.style.left = `${e.pageX + 10}px`;
-      cursorCoordsText.style.top = `${e.pageY + 10}px`;
-    });
-  
     document.querySelectorAll('area').forEach(area => {
-      const overlay = createCircleOverlay(area.getAttribute('coords'));
-      imageContainer.appendChild(overlay);
+      area.addEventListener('click', function(e) {
+        e.preventDefault();
+        const zone = this.dataset.zone;
+        checkSeatStatus(zone);
+      });
     });
   });
+  
